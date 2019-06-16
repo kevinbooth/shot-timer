4456882ef4642cb629553a5407c11909202bafd3
Kevin Booth <kdbooth97@gmail.com>
2019-06-16 11:00:08 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img src={logo} className="app__logo" alt="logo" />
+      <button className="button">Start timer</button>
     </div>
   );
 }
